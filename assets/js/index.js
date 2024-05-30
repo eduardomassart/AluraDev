e04e81c451e769b0c9f23af194f0f5040f5c9ee1
Eduardo Bartichoto Massare <eduardobarti@gmail.com>
2024-05-30 02:05:21 -0300

    Ajuste da área de edição de código com css (tamanho limite, scrollbar, quebra de linha) e aplicação de syntax highlight com click de botão

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+const linguagem = document.querySelector(".input__projeto-linguagem");
+const areaDoCodigo = document.querySelector(".code-wrapper");
+const botao = document.querySelector(".syntax-highlight");
+
+function aplicaHighlight () {
+    const codigo = areaDoCodigo.innerText;
+    areaDoCodigo.innerHTML = `<code class="coding__area linguagem-${linguagem.value}" contenteditable="true" aria-label="Editor de Código"></code>`
+    areaDoCodigo.querySelector("code").textContent = codigo;
+    hljs.highlightElement(areaDoCodigo.querySelector("code"));
+}
+
+botao.addEventListener("click", () => {
+    aplicaHighlight();
+})
